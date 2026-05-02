dda199bde3be75eee1455de140997d90d8a43997
khatereh <khaterehnaseri444@gmail.com>
2026-05-02 17:59:38 +0330

    feat : create modal costum hook and use it

--- vite/src/core/hooks/modal/Modal.jsx
+++ vite/src/core/hooks/modal/Modal.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Modal({ openModal, setOpenModal }) {
+function Modal({ openModal, setOpenModal, children }) {
   return (
     openModal && (
-        <div >hello world</div>
+      <div
+        onClick={() => setOpenModal(false)}
+        className="w-full h-200  backdrop-blur-md bg-[#0000007A] inset-0 fixed z-50 flex items-center justify-center"
+      >
+        {children}
+      </div>
     )
-  )
+  );
 }
 
 export default Modal;
--- vite/src/pages/ModalTest.jsx
+++ vite/src/pages/ModalTest.jsx
@@ -5,10 +5,31 @@ function ModalTest() {
   const [openModal, setOpenModal] = useState(false);
   console.log(openModal);
   return (
-    <div className="w-full h-200 bg-green-400">
-      <h1>Modal Test</h1>
-      <button onClick={() => setOpenModal(!openModal)}>*</button>
-      {openModal && <Modal openModal={openModal} setOpenModal={setOpenModal} />}
+    <div className="w-full h-200 bg-pink-200 flex items-center justify-center flex-col gap-20">
+      <h1 className="text-[30px] font-bold">Modal Test</h1>
+      <button
+        onClick={() => setOpenModal(!openModal)}
+        className="w-12 h-12 rounded-full bg-pink-500 flex items-center justify-center text-[15px] cursor-grab"
+      >
+        Click
+      </button>
+      {openModal && (
+        <Modal openModal={openModal} setOpenModal={setOpenModal}>
+          <div className="w-200 h-100 bg-white opacity-100 flex items-center justify-center gap-20 p-5 flex-col">
+            <div className="w-full h-10 flex items-center justify-end">
+              <button
+                onClick={() => setOpenModal(false)}
+                className="w-8 h-8 bg-red-600 flex items-center justify-center cursor-pointer"
+              >
+                X
+              </button>
+            </div>
+            <p className="w-full h-80">
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed molestias eum fugit ab non quia error! Consequuntur, quam? Dolor, quod voluptas! Ipsa, nesciunt commodi. Recusandae modi suscipit dolorum, iure blanditiis odit enim perferendis unde alias, possimus corrupti! Sequi modi culpa explicabo totam minus labore, reiciendis iste quaerat incidunt atque? Ut aliquam quos labore molestiae? Ducimus illum enim vero sunt ab iure voluptas, ad quia similique officia laudantium voluptatem odio? Non, blanditiis? In similique inventore eum. Odio sapiente quaerat, ut perferendis officiis illum dignissimos praesentium quam et eaque molestiae sit, deserunt numquam minima delectus beatae veniam quibusdam dolorum voluptatibus facere! Ducimus?
+            </p>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
